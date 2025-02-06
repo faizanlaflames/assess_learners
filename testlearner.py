f655efeeafd0112652849c2c9df3e42ab7b2d05d
faizanlaflames <fhussai5@asu.edu>
2025-02-05 17:07:00 -0800

    generating the correct plots

--- testlearner.py
+++ testlearner.py
@@ -23,7 +23,6 @@ GT honor code violation.
 -----do not edit anything above this line---  		  	   		 	 	 			  		 			     			  	 
 """  		  	   		 	 	 			  		 			     			  	 
   		  	   		 	 	 			  		 			     			  	 
-import math
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
@@ -31,93 +30,147 @@ import DTLearner as dt
 import RTLearner as rt
 import BagLearner as bl
 import InsaneLearner as il
+import time
+import sys
 
-def run_experiment(data_file):
-    # load data, skip header row
+def experiment1(data_file):
     data = np.genfromtxt(data_file, delimiter=',')
-    data = data[1:, 1:]
-
-    # split into training/test sets (60/40)
-    train_rows = int(0.6 * data.shape[0])
-    train_x = data[:train_rows, :-1]
-    train_y = data[:train_rows, -1]
-    test_x = data[train_rows:, :-1]
-    test_y = data[train_rows:, -1]
-
-    # initialize different learner types
-    dt_learner = dt.DTLearner(leaf_size=1, verbose=False)
-    rt_learner = rt.RTLearner(leaf_size=1, verbose=False)
-    bag_learner = bl.BagLearner(learner=rt.RTLearner, kwargs={"leaf_size": 1}, bags=20, boost=False, verbose=False)
-    insane_learner = il.InsaneLearner(verbose=False)
-
-    # train all models on training data
-    dt_learner.add_evidence(train_x, train_y)
-    rt_learner.add_evidence(train_x, train_y)
-    bag_learner.add_evidence(train_x, train_y)
-    insane_learner.add_evidence(train_x, train_y)
-
-    # calculate in-sample metrics
-    dt_pred_train = dt_learner.query(train_x)
-    rt_pred_train = rt_learner.query(train_x)
-    bag_pred_train = bag_learner.query(train_x)
-    insane_pred_train = insane_learner.query(train_x)
-
-    dt_rmse_train = np.sqrt(((train_y - dt_pred_train) ** 2).mean())
-    rt_rmse_train = np.sqrt(((train_y - rt_pred_train) ** 2).mean())
-    bag_rmse_train = np.sqrt(((train_y - bag_pred_train) ** 2).mean())
-    insane_rmse_train = np.sqrt(((train_y - insane_pred_train) ** 2).mean())
-
-    dt_corr_train = np.corrcoef(train_y, dt_pred_train)[0, 1]
-    rt_corr_train = np.corrcoef(train_y, rt_pred_train)[0, 1]
-    bag_corr_train = np.corrcoef(train_y, bag_pred_train)[0, 1]
-    insane_corr_train = np.corrcoef(train_y, insane_pred_train)[0, 1]
-
-    # calculate out-of-sample metrics
-    dt_pred_test = dt_learner.query(test_x)
-    rt_pred_test = rt_learner.query(test_x)
-    bag_pred_test = bag_learner.query(test_x)
-    insane_pred_test = insane_learner.query(test_x)
-
-    dt_rmse_test = np.sqrt(((test_y - dt_pred_test) ** 2).mean())
-    rt_rmse_test = np.sqrt(((test_y - rt_pred_test) ** 2).mean())
-    bag_rmse_test = np.sqrt(((test_y - bag_pred_test) ** 2).mean())
-    insane_rmse_test = np.sqrt(((test_y - insane_pred_test) ** 2).mean())
-
-    dt_corr_test = np.corrcoef(test_y, dt_pred_test)[0, 1]
-    rt_corr_test = np.corrcoef(test_y, rt_pred_test)[0, 1]
-    bag_corr_test = np.corrcoef(test_y, bag_pred_test)[0, 1]
-    insane_corr_test = np.corrcoef(test_y, insane_pred_test)[0, 1]
-
-    # print training performance
-    print("In-Sample Results:")
-    print(f"DTLearner RMSE: {dt_rmse_train}, Corr: {dt_corr_train}")
-    print(f"RTLearner RMSE: {rt_rmse_train}, Corr: {rt_corr_train}")
-    print(f"BagLearner RMSE: {bag_rmse_train}, Corr: {bag_corr_train}")
-    print(f"InsaneLearner RMSE: {insane_rmse_train}, Corr: {insane_corr_train}")
-
-    # print testing performance  
-    print("\nOut-of-Sample Results:")
-    print(f"DTLearner RMSE: {dt_rmse_test}, Corr: {dt_corr_test}")
-    print(f"RTLearner RMSE: {rt_rmse_test}, Corr: {rt_corr_test}")
-    print(f"BagLearner RMSE: {bag_rmse_test}, Corr: {bag_corr_test}")
-    print(f"InsaneLearner RMSE: {insane_rmse_test}, Corr: {insane_corr_test}")
-
-    # create rmse comparison chart
-    labels = ['DTLearner', 'RTLearner', 'BagLearner', 'InsaneLearner']
-    rmse_train = [dt_rmse_train, rt_rmse_train, bag_rmse_train, insane_rmse_train]
-    rmse_test = [dt_rmse_test, rt_rmse_test, bag_rmse_test, insane_rmse_test]
+    data = data[1:, 1:] 
     
-    plt.figure(figsize=(10, 5))
-    plt.bar(labels, rmse_train, label='In-Sample RMSE')
-    plt.bar(labels, rmse_test, label='Out-of-Sample RMSE', alpha=0.5)
-    plt.title('RMSE Comparison of Learners')
+    leaf_sizes = range(1, 51)
+    in_sample_rmse = []
+    out_sample_rmse = []
+    
+    for ls in leaf_sizes:
+        np.random.seed(903471054) 
+        np.random.shuffle(data)
+        train_rows = int(0.6 * data.shape[0])
+        train_x, train_y = data[:train_rows, :-1], data[:train_rows, -1]
+        test_x, test_y = data[train_rows:, :-1], data[train_rows:, -1]
+        
+        learner = dt.DTLearner(leaf_size=ls, verbose=False)
+        learner.add_evidence(train_x, train_y)
+        
+        pred_train = learner.query(train_x)
+        pred_test = learner.query(test_x)
+        in_sample_rmse.append(np.sqrt(((train_y - pred_train)**2).mean()))
+        out_sample_rmse.append(np.sqrt(((test_y - pred_test)**2).mean()))
+    
+    plt.figure(figsize=(10, 6))
+    plt.plot(leaf_sizes, in_sample_rmse, label='In-Sample RMSE')
+    plt.plot(leaf_sizes, out_sample_rmse, label='Out-of-Sample RMSE')
+    plt.title('DTLearner Overfitting Analysis')
+    plt.xlabel('Leaf Size')
     plt.ylabel('RMSE')
     plt.legend()
-    plt.savefig('rmse_comparison.png')
-    plt.show()
+    plt.grid(True)
+    plt.savefig('experiment1_overfitting.png')
+    plt.close()
+
+def experiment2(data_file):
+    data = np.genfromtxt(data_file, delimiter=',')
+    data = data[1:, 1:]  
+    
+    leaf_sizes = range(1, 51)
+    bag_rmse = []
+    single_rmse = []
+    
+    for ls in leaf_sizes:
+        np.random.seed(904082279)  
+        np.random.shuffle(data)
+        train_rows = int(0.6 * data.shape[0])
+        train_x, train_y = data[:train_rows, :-1], data[:train_rows, -1]
+        test_x, test_y = data[train_rows:, :-1], data[train_rows:, -1]
+        
+        bag_learner = bl.BagLearner(learner=dt.DTLearner, kwargs={"leaf_size": ls}, bags=20, boost=False, verbose=False)
+        bag_learner.add_evidence(train_x, train_y)
+        pred_bag = bag_learner.query(test_x)
+        bag_rmse.append(np.sqrt(((test_y - pred_bag)**2).mean()))
+        
+        single_learner = dt.DTLearner(leaf_size=ls, verbose=False)
+        single_learner.add_evidence(train_x, train_y)
+        pred_single = single_learner.query(test_x)
+        single_rmse.append(np.sqrt(((test_y - pred_single)**2).mean()))
+    
+    plt.figure(figsize=(10, 6))
+    plt.plot(leaf_sizes, bag_rmse, label='Bagged DTLearner')
+    plt.plot(leaf_sizes, single_rmse, label='Single DTLearner')
+    plt.title('Bagging Effect on Overfitting')
+    plt.xlabel('Leaf Size')
+    plt.ylabel('Out-of-Sample RMSE')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig('experiment2_bagging.png')
+    plt.close()
+
+def experiment3(data_file):
+    data = np.genfromtxt(data_file, delimiter=',')
+    data = data[1:, 1:] 
+    
+    metrics = {
+        'DT_MAE': [],
+        'RT_MAE': [],
+        'DT_Time': [],
+        'RT_Time': []
+    }
+    
+    leaf_sizes = range(1, 51)
+    
+    for ls in leaf_sizes:
+        np.random.seed(904082279) 
+        np.random.shuffle(data)
+        train_rows = int(0.6 * data.shape[0])
+        train_x, train_y = data[:train_rows, :-1], data[:train_rows, -1]
+        test_x, test_y = data[train_rows:, :-1], data[train_rows:, -1]
+        
+        start_time = time.time()
+        dt_learner = dt.DTLearner(leaf_size=ls, verbose=False)
+        dt_learner.add_evidence(train_x, train_y)
+        dt_time = time.time() - start_time
+        dt_pred = dt_learner.query(test_x)
+        dt_mae = np.abs(test_y - dt_pred).mean()
+        
+        start_time = time.time()
+        rt_learner = rt.RTLearner(leaf_size=ls, verbose=False)
+        rt_learner.add_evidence(train_x, train_y)
+        rt_time = time.time() - start_time
+        rt_pred = rt_learner.query(test_x)
+        rt_mae = np.abs(test_y - rt_pred).mean()
+        
+        metrics['DT_MAE'].append(dt_mae)
+        metrics['RT_MAE'].append(rt_mae)
+        metrics['DT_Time'].append(dt_time)
+        metrics['RT_Time'].append(rt_time)
+    
+    plt.figure(figsize=(10, 6))
+    plt.plot(leaf_sizes, metrics['DT_MAE'], label='DTLearner MAE')
+    plt.plot(leaf_sizes, metrics['RT_MAE'], label='RTLearner MAE')
+    plt.title('MAE Comparison (DT vs RT)')
+    plt.xlabel('Leaf Size')
+    plt.ylabel('Mean Absolute Error')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig('experiment3_mae.png')
+    plt.close()
+    
+    plt.figure(figsize=(10, 6))
+    plt.plot(leaf_sizes, metrics['DT_Time'], label='DTLearner Training Time')
+    plt.plot(leaf_sizes, metrics['RT_Time'], label='RTLearner Training Time')
+    plt.title('Training Time Comparison')
+    plt.xlabel('Leaf Size')
+    plt.ylabel('Time (seconds)')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig('experiment3_time.png')
+    plt.close()
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python testlearner.py <filename>")
         sys.exit(1)
-    run_experiment(sys.argv[1])
+    
+    data_file = sys.argv[1]
+    experiment1(data_file)
+    experiment2(data_file)
+    experiment3(data_file)
+    print("Experiments completed. Check the generated PNG files for results.")
