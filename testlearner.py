2b86207dfe48c9266b7dfabc2c7cf12c705bc5ac
faizanlaflames <fhussai5@asu.edu>
2025-02-09 01:28:54 -0800

    fixing chart generation

--- testlearner.py
+++ testlearner.py
@@ -1,28 +1,26 @@
-""""""  		  	   		 	 	 			  		 			     			  	 
-"""  		  	   		 	 	 			  		 			     			  	 
-Test a learner.  (c) 2015 Tucker Balch  		  	   		 	 	 			  		 			     			  	 
-  		  	   		 	 	 			  		 			     			  	 
-Copyright 2018, Georgia Institute of Technology (Georgia Tech)  		  	   		 	 	 			  		 			     			  	 
-Atlanta, Georgia 30332  		  	   		 	 	 			  		 			     			  	 
-All Rights Reserved  		  	   		 	 	 			  		 			     			  	 
-  		  	   		 	 	 			  		 			     			  	 
-Template code for CS 4646/7646  		  	   		 	 	 			  		 			     			  	 
-  		  	   		 	 	 			  		 			     			  	 
-Georgia Tech asserts copyright ownership of this template and all derivative  		  	   		 	 	 			  		 			     			  	 
-works, including solutions to the projects assigned in this course. Students  		  	   		 	 	 			  		 			     			  	 
-and other users of this template code are advised not to share it with others  		  	   		 	 	 			  		 			     			  	 
-or to make it available on publicly viewable websites including repositories  		  	   		 	 	 			  		 			     			  	 
-such as github and gitlab.  This copyright statement should not be removed  		  	   		 	 	 			  		 			     			  	 
-or edited.  		  	   		 	 	 			  		 			     			  	 
-  		  	   		 	 	 			  		 			     			  	 
-We do grant permission to share solutions privately with non-students such  		  	   		 	 	 			  		 			     			  	 
-as potential employers. However, sharing with other current or future  		  	   		 	 	 			  		 			     			  	 
-students of CS 7646 is prohibited and subject to being investigated as a  		  	   		 	 	 			  		 			     			  	 
-GT honor code violation.  		  	   		 	 	 			  		 			     			  	 
-  		  	   		 	 	 			  		 			     			  	 
------do not edit anything above this line---  		  	   		 	 	 			  		 			     			  	 
-"""  		  	   		 	 	 			  		 			     			  	 
-  		  	   		 	 	 			  		 			     			  	 
+"""test a learner.  (c) 2015 tucker balch
+
+copyright 2018, georgia institute of technology (georgia tech)
+atlanta, georgia 30332
+all rights reserved
+
+template code for cs 4646/7646
+
+georgia tech asserts copyright ownership of this template and all derivative
+works, including solutions to the projects assigned in this course. students
+and other users of this template code are advised not to share it with others
+or to make it available on publicly viewable websites including repositories
+such as github and gitlab.  this copyright statement should not be removed
+or edited.
+
+we do grant permission to share solutions privately with non-students such
+as potential employers. however, sharing with other current or future
+students of cs 7646 is prohibited and subject to being investigated as a
+gt honor code violation.
+
+-----do not edit anything above this line---
+"""
+
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
@@ -34,19 +32,27 @@ import time
 import sys
 
 def experiment1(data_file):
+   
+    np.random.seed(903471054) 
+    
     data = np.genfromtxt(data_file, delimiter=',')
-    data = data[1:, 1:] 
+    data = data[1:, 1:]  
     
     leaf_sizes = range(1, 51)
     in_sample_rmse = []
     out_sample_rmse = []
     
+    # split data once before the loop
+    train_rows = int(0.6 * data.shape[0])
+    np.random.shuffle(data)
+    train_data = data[:train_rows]
+    test_data = data[train_rows:]
+    
     for ls in leaf_sizes:
-        np.random.seed(903471054) 
-        np.random.shuffle(data)
-        train_rows = int(0.6 * data.shape[0])
-        train_x, train_y = data[:train_rows, :-1], data[:train_rows, -1]
-        test_x, test_y = data[train_rows:, :-1], data[train_rows:, -1]
+        train_x = train_data[:, :-1]
+        train_y = train_data[:, -1]
+        test_x = test_data[:, :-1]
+        test_y = test_data[:, -1]
         
         learner = dt.DTLearner(leaf_size=ls, verbose=False)
         learner.add_evidence(train_x, train_y)
@@ -56,10 +62,11 @@ def experiment1(data_file):
         in_sample_rmse.append(np.sqrt(((train_y - pred_train)**2).mean()))
         out_sample_rmse.append(np.sqrt(((test_y - pred_test)**2).mean()))
     
+    # reverse the x-axis for leaf sizes
     plt.figure(figsize=(10, 6))
     plt.plot(leaf_sizes, in_sample_rmse, label='In-Sample RMSE')
     plt.plot(leaf_sizes, out_sample_rmse, label='Out-of-Sample RMSE')
-    plt.title('DTLearner Overfitting Analysis')
+    plt.title('Experiment 1: DTLearner RMSE by Leaf Size')
     plt.xlabel('Leaf Size')
     plt.ylabel('RMSE')
     plt.legend()
@@ -68,45 +75,72 @@ def experiment1(data_file):
     plt.close()
 
 def experiment2(data_file):
+    
+    np.random.seed(903471054) 
+    
     data = np.genfromtxt(data_file, delimiter=',')
-    data = data[1:, 1:]  
+    data = data[1:, 1:]
+    
+    # split data once before the loop
+    train_rows = int(0.6 * data.shape[0])
+    np.random.shuffle(data)
+    train_data = data[:train_rows]
+    test_data = data[train_rows:]
     
     leaf_sizes = range(1, 51)
     bag_rmse = []
     single_rmse = []
     
     for ls in leaf_sizes:
-        np.random.seed(904082279)  
-        np.random.shuffle(data)
-        train_rows = int(0.6 * data.shape[0])
-        train_x, train_y = data[:train_rows, :-1], data[:train_rows, -1]
-        test_x, test_y = data[train_rows:, :-1], data[train_rows:, -1]
+        train_x = train_data[:, :-1]
+        train_y = train_data[:, -1]
+        test_x = test_data[:, :-1]
+        test_y = test_data[:, -1]
         
-        bag_learner = bl.BagLearner(learner=dt.DTLearner, kwargs={"leaf_size": ls}, bags=20, boost=False, verbose=False)
-        bag_learner.add_evidence(train_x, train_y)
-        pred_bag = bag_learner.query(test_x)
-        bag_rmse.append(np.sqrt(((test_y - pred_bag)**2).mean()))
-        
+        # single dtlearner
         single_learner = dt.DTLearner(leaf_size=ls, verbose=False)
         single_learner.add_evidence(train_x, train_y)
         pred_single = single_learner.query(test_x)
         single_rmse.append(np.sqrt(((test_y - pred_single)**2).mean()))
+        
+        # bagged dtlearner
+        bag_learner = bl.BagLearner(
+            learner=dt.DTLearner,
+            kwargs={"leaf_size": ls},
+            bags=20,
+            boost=False,
+            verbose=False
+        )
+        bag_learner.add_evidence(train_x, train_y)
+        pred_bag = bag_learner.query(test_x)
+        bag_rmse.append(np.sqrt(((test_y - pred_bag)**2).mean()))
     
+    # plot with reversed x-axis like experiment 1
     plt.figure(figsize=(10, 6))
     plt.plot(leaf_sizes, bag_rmse, label='Bagged DTLearner')
     plt.plot(leaf_sizes, single_rmse, label='Single DTLearner')
-    plt.title('Bagging Effect on Overfitting')
+    plt.title('Experiment 2: Bagging Effect on Overfitting (DTLearner)')
     plt.xlabel('Leaf Size')
-    plt.ylabel('Out-of-Sample RMSE')
+    plt.ylabel('RMSE')
     plt.legend()
     plt.grid(True)
     plt.savefig('experiment2_bagging.png')
     plt.close()
 
 def experiment3(data_file):
-    data = np.genfromtxt(data_file, delimiter=',')
-    data = data[1:, 1:] 
     
+    np.random.seed(903471054)  
+    
+    data = np.genfromtxt(data_file, delimiter=',')
+    data = data[1:, 1:]
+    
+    # split data once before the loop
+    train_rows = int(0.6 * data.shape[0])
+    np.random.shuffle(data)
+    train_data = data[:train_rows]
+    test_data = data[train_rows:]
+    
+    leaf_sizes = range(1, 51)
     metrics = {
         'DT_MAE': [],
         'RT_MAE': [],
@@ -114,28 +148,27 @@ def experiment3(data_file):
         'RT_Time': []
     }
     
-    leaf_sizes = range(1, 51)
-    
     for ls in leaf_sizes:
-        np.random.seed(904082279) 
-        np.random.shuffle(data)
-        train_rows = int(0.6 * data.shape[0])
-        train_x, train_y = data[:train_rows, :-1], data[:train_rows, -1]
-        test_x, test_y = data[train_rows:, :-1], data[train_rows:, -1]
+        train_x = train_data[:, :-1]
+        train_y = train_data[:, -1]
+        test_x = test_data[:, :-1]
+        test_y = test_data[:, -1]
         
+        # dtlearner metrics
         start_time = time.time()
         dt_learner = dt.DTLearner(leaf_size=ls, verbose=False)
         dt_learner.add_evidence(train_x, train_y)
         dt_time = time.time() - start_time
         dt_pred = dt_learner.query(test_x)
-        dt_mae = np.abs(test_y - dt_pred).mean()
+        dt_mae = np.mean(np.abs(test_y - dt_pred))
         
+        # rtlearner metrics
         start_time = time.time()
         rt_learner = rt.RTLearner(leaf_size=ls, verbose=False)
         rt_learner.add_evidence(train_x, train_y)
         rt_time = time.time() - start_time
         rt_pred = rt_learner.query(test_x)
-        rt_mae = np.abs(test_y - rt_pred).mean()
+        rt_mae = np.mean(np.abs(test_y - rt_pred))
         
         metrics['DT_MAE'].append(dt_mae)
         metrics['RT_MAE'].append(rt_mae)
@@ -145,7 +178,7 @@ def experiment3(data_file):
     plt.figure(figsize=(10, 6))
     plt.plot(leaf_sizes, metrics['DT_MAE'], label='DTLearner MAE')
     plt.plot(leaf_sizes, metrics['RT_MAE'], label='RTLearner MAE')
-    plt.title('MAE Comparison (DT vs RT)')
+    plt.title('Experiment 3: MAE Comparison (DT vs RT)')
     plt.xlabel('Leaf Size')
     plt.ylabel('Mean Absolute Error')
     plt.legend()
@@ -153,16 +186,18 @@ def experiment3(data_file):
     plt.savefig('experiment3_mae.png')
     plt.close()
     
+    # plot training time comparison
     plt.figure(figsize=(10, 6))
     plt.plot(leaf_sizes, metrics['DT_Time'], label='DTLearner Training Time')
     plt.plot(leaf_sizes, metrics['RT_Time'], label='RTLearner Training Time')
-    plt.title('Training Time Comparison')
+    plt.title('Experiment 3: Training Time Comparison')
     plt.xlabel('Leaf Size')
     plt.ylabel('Time (seconds)')
     plt.legend()
     plt.grid(True)
     plt.savefig('experiment3_time.png')
     plt.close()
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -174,3 +209,4 @@ if __name__ == "__main__":
     experiment2(data_file)
     experiment3(data_file)
     print("Experiments completed. Check the generated PNG files for results.")
+    
